5a525bcbaab8553b5567d8df4f7cceea6009c9d9
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-28 12:13:35 +0000

    added test for render

--- src/Posts.js
+++ src/Posts.js
@@ -26,7 +26,7 @@ class Posts extends Component {
     })
 
     return (
-      <ul class="posts">
+      <ul>
         {posts}
       </ul>
     )
--- src/Posts.test.js
+++ src/Posts.test.js
@@ -5,14 +5,19 @@ import Posts from './Posts'
 import fetchPosts from './fetchPosts'
 
 configure({ adapter: new Adapter() });
+  window.fetch = fetchPosts()
 
 describe('Posts', () => {
   it('should fetch posts', async () => {
-
-    window.fetch = fetchPosts()
-
     const component = await mount(<Posts />)
     await component.update()
     expect(component.state('posts').length).toEqual(2);
+  });
+
+  it('renders each post', async () => {
+    const component = await mount(<Posts />)
+    await component.update()
+
+    expect(component.text()).toContain('message 1', "message 2");
   });
 });
