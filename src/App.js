0987d3ef777265e7502287af798ab2938fb50b8a
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-29 15:55:13 +0000

    users can fill in sign up form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PostsBody from './PostsBody'
+import LoginBody from './LoginBody'
 import SignUpBody from './SignUpBody'
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
           <Switch>
+            <Route exact path='/login' component={LoginBody}/>
             <Route exact path='/posts' component={PostsBody}/>
             <Route exact path='/' component={SignUpBody}/>
           </Switch>
--- src/LoginBody.js
+++ src/LoginBody.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-class SignUpBody extends Component {
+
+class LoginBody extends Component {
   constructor(props) {
     super(props)
   }
 
-  handleFormSumbit(name, email, password) {
-    let body = JSON.stringify({user: {name: name, email: email, password: password}})
-    fetch('https://maicers-acebook-api.herokuapp.com/signup' , {
+  handleLoginSumbit(email, password) {
+    let body = JSON.stringify({user: {email: email, password: password}})
+
+    fetch('https://maicers-acebook-api.herokuapp.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -18,18 +20,18 @@ class SignUpBody extends Component {
 
   render() {
     let formFields = {}
-    return(
+
+    return (
       <form>
-        <input placeholder="Enter name" id="name" ref={input => formFields.name = input} type="text"/><br/>
         <input placeholder="Enter email" id="email" ref={input => formFields.email = input} type="email"/><br/>
         <input placeholder="Enter password" id="password" ref={input => formFields.password = input} type="password"/><br/>
-        <input placeholder="Confirm Password" id="passwordconfirmation" type="password"/><br/>
         <button onClick={ (e) => {
-          this.handleFormSumbit(formFields.name.value, formFields.email.value, formFields.password.value)
+          this.handleLoginSumbit(formFields.email.value, formFields.password.value)
         }}>Sign Up</button>
       </form>
     )
   }
+
 }
 
-export default SignUpBody
+export default LoginBody
