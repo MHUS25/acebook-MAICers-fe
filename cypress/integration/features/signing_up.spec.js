4740a9c9f38a3cfea280c29e729628191bc0a6c4
Muna <mmhussein125@gmail.com>
2018-11-29 14:47:01 +0000

    sign up implemented

--- cypress/integration/features/signing_up.spec.js
+++ cypress/integration/features/signing_up.spec.js
@@ -1,10 +1,29 @@
+const deferred = require('./deferred')
+
 describe('Signing up', function () {
+
+  beforeEach(function() {
+    this.fetchPostsDeferred = deferred()
+
+    cy.visit('/signup', {
+      onBeforeLoad (win) {
+        cy.stub(win, 'fetch')
+        .withArgs('https://maicers-acebook-api.herokuapp.com/signup')
+        .as('postUser')
+        .returns(this.fetchPostsDeferred.promise)
+      }
+    });
+  })
+
+
   it ('has a sign up form', function() {
-    cy.visit('/signup')
     cy.get('#name').type('Clark Kent')
     cy.get('#email').type('Clark.Kent@test.com')
     cy.get('#password').type('superman')
     cy.get('#passwordconfirmation').type('superman')
     cy.get('button').click()
+    cy.get('@postUser').should('be.calledWith', 'https://maicers-acebook-api.herokuapp.com/signup')
   });
+
+
 });
--- src/SignUpBody.js
+++ src/SignUpBody.js
@@ -5,14 +5,29 @@ class SignUpBody extends Component {
     super(props)
   }
 
+  handleFormSumbit(name, email, password) {
+    console.log(name, email, password)
+    let body = JSON.stringify({user: {name: name, email: email, password: password}})
+    fetch('https://maicers-acebook-api.herokuapp.com/signup' , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    })
+  }
+
   render() {
+    let formFields = {}
     return(
       <form>
-        <input id="name" type="text"/>
-        <input id="email" type="email"/>
-        <input id="password" type="password"/>
+        <input id="name" ref={input => formFields.name = input} type="text"/>
+        <input id="email" ref={input => formFields.email = input} type="email"/>
+        <input id="password" ref={input => formFields.password = input} type="password"/>
         <input id="passwordconfirmation" type="password"/>
-        <button>Sign Up</button>
+        <button onClick={ (e) => {
+          this.handleFormSumbit(formFields.name.value, formFields.email.value, formFields.password.value)
+        }}>Sign Up</button>
       </form>
     )
   }
