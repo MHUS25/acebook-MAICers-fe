09aa230fa50a833af6d42706ac0364b355ee635b
Muna <mmhussein125@gmail.com>
2018-11-30 11:53:56 +0000

    adds basic welcome page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PostsBody from './PostsBody'
+import Welcome from './Welcome'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
           <Switch>
             <Route exact path='/posts' component={PostsBody}/>
+            <Route exact path='/' component={Welcome}/>
           </Switch>
       </div>
     );
